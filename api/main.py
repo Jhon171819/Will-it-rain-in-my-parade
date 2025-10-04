cfb28ec58875ce9a71f36734e40219fa77cf90fb
18juba <jubalucas05@gmail.com>
2025-10-04 14:58:01 -0300

    Simple local info endpoint

--- api/main.py
+++ api/main.py
@@ -1,11 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from services.nominatim.NominatimService import NominatimService
 
 app = FastAPI()
 
+@app.get("/local_info")
+def get_local_info(lat: str = Query(..., description="Latitude"), lon: str = Query(..., description="Longitude")):
+    address = {
+        "location": NominatimService.LocationData(lat, lon),
+    }
+    return address
+
+
 @app.get("/dashboard")
-def get_dashboard():
+def get_dashboard(lat: str = Query(..., description="Latitude"), lon: str = Query(..., description="Longitude")):
     dashboard_data = {
-        "location_card": NominatimService.LocationData("-7.8725687", "-77.3607387"),
+        "data": {
+            "location_card": NominatimService.LocationData(lat, lon),
+        }
     }
     return dashboard_data
