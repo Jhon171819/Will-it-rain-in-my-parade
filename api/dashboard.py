d90c201b5d211cc3622738082efbbcc2b2add5a8
18juba <jubalucas05@gmail.com>
2025-10-04 14:05:46 -0300

    Return simple dashboard data

--- api/dashboard.py
+++ api/dashboard.py
@@ -6,28 +6,25 @@ from services.nominatim.NominatimService import NominatimService
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        # Extrai parâmetros da URL
         parsed_url = urlparse(self.path)
         query_params = parse_qs(parsed_url.query)
         lat = query_params.get("lat", [None])[0]
         lon = query_params.get("lon", [None])[0]
 
-        # Validação
         if not lat or not lon:
             self.send_response(400)
             self.send_header("Content-type", "application/json")
             self.end_headers()
-            message = {"error": "Parâmetros 'lat' e 'lon' são obrigatórios"}
+            message = {"error": "'lat' and 'lon' parameters are required"}
             self.wfile.write(json.dumps(message).encode())
             return
 
-        # Executa a busca pelo endereço
         try:
-            address = NominatimService.reverse_geocode(lat, lon)
+            address = NominatimService.LocationData(lat, lon)
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
-            self.wfile.write(json.dumps({"address": address}).encode())
+            self.wfile.write(json.dumps({"data": address}).encode())
         except Exception as e:
             self.send_response(500)
             self.send_header("Content-type", "application/json")
